f85b1c08713f4b081b4b01f8ec4d33aeabd484ee
serj <srdjan@adjust.com>
2020-04-01 21:28:20 +0200

    Initial Commit

---
+++ TerminalBuddyAgent/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  TerminalBuddyAgent
+//
+//  Created by Srdjan Tubin on 01.04.20.
+//  Copyright © 2020 Srdjan Tubin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
