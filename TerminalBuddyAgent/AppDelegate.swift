40d6487ebe3c7609caa07874a3a95549413d1574
serj <srdjan@adjust.com>
2020-04-13 17:59:42 +0200

    added WS lib Starscream

--- TerminalBuddyAgent/AppDelegate.swift
+++ TerminalBuddyAgent/AppDelegate.swift
@@ -8,9 +8,13 @@
 
 import Cocoa
 import SwiftUI
+import Starscream
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
+class AppDelegate: NSObject, NSApplicationDelegate, WebSocketDelegate {
+    var socket: WebSocket!
+    var isConnected = false
+    let server = WebSocketServer()
 
     // taken from:
     // https://medium.com/@acwrightdesign/creating-a-macos-menu-bar-application-using-swiftui-54572a5d5f87
@@ -36,6 +40,14 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             button.image = NSImage(named: "Icon")
             button.action = #selector(togglePopover(_:))
         }
+        
+        var request = URLRequest(url: URL(string: "ws://localhost:8080/connect")!)
+        request.timeoutInterval = 5
+        socket = WebSocket(request: request)
+        socket.delegate = self
+        socket.connect()
+        
+        // showNotification(title: "term buddy test", subtitle: "test message")
     }
     
     @objc func togglePopover(_ sender: AnyObject?) {
@@ -48,5 +60,69 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
     
+    // MARK: - WebSocketDelegate
+    func didReceive(event: WebSocketEvent, client: WebSocket) {
+        switch event {
+        case .connected(let headers):
+            isConnected = true
+            print("websocket is connected: \(headers)")
+        case .disconnected(let reason, let code):
+            isConnected = false
+            print("websocket is disconnected: \(reason) with code: \(code)")
+        case .text(let string):
+            print("Received text: \(string)")
+            showNotification(title: "term buddy test", subtitle: string)
+        case .binary(let data):
+            print("Received data: \(data.count)")
+        case .ping(_):
+            break
+        case .pong(_):
+            break
+        case .viabilityChanged(_):
+            break
+        case .reconnectSuggested(_):
+            break
+        case .cancelled:
+            isConnected = false
+        case .error(let error):
+            isConnected = false
+            handleError(error)
+        }
+    }
+    
+    func handleError(_ error: Error?) {
+        if let e = error as? WSError {
+            print("websocket encountered an error: \(e.message)")
+        } else if let e = error {
+            print("websocket encountered an error: \(e.localizedDescription)")
+        } else {
+            print("websocket encountered an error")
+        }
+    }
+    
+//    // MARK: Write Text Action
+//    @IBAction func writeText(_ sender: UIBarButtonItem) {
+//        socket.write(string: "hello there!")
+//    }
+//
+//    // MARK: Disconnect Action
+//    @IBAction func disconnect(_ sender: UIBarButtonItem) {
+//        if isConnected {
+//            sender.title = "Connect"
+//            socket.disconnect()
+//        } else {
+//            sender.title = "Disconnect"
+//            socket.connect()
+//        }
+//    }
+    
+    func showNotification(title: String, subtitle: String) -> Void {
+        let notification = NSUserNotification()
+        notification.deliveryDate = Date(timeIntervalSinceNow: 3)
+        notification.title = title
+        notification.subtitle = subtitle
+        notification.soundName = NSUserNotificationDefaultSoundName
+        NSUserNotificationCenter.default.scheduleNotification(notification)
+    }
 }
 
